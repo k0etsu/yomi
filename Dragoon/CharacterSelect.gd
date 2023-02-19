267e84eb3768cdd970869c6d33a67e9b47a2ac7f
Crito <DeviousCrypto@gmail.com>
2023-02-19 17:32:29 -0600

    init

---
+++ Dragoon/CharacterSelect.gd
@@ -0,0 +1,6 @@
+extends "res://ui/CSS/CharacterSelect.gd"
+
+func _ready():
+	addCustomChar("Dragoon", "res://Dragoon/characters/Dragoon/Dragoon.tscn")
+	
+
---
+++ Dragoon/modmain.gd
@@ -0,0 +1,6 @@
+extends Node
+
+func _init(ml = ModLoader):
+	ml.installScriptExtension("res://Dragoon/CharacterSelect.gd")
+	
+
